14540cdf6ddbf9354ef9eba65e6e1f968a7220c5
Sahil Shah <s.shah1357@gmail.com>
2012-03-02 17:42:08 -0800

    filled out relations migration

--- db/migrate/20120303012729_create_relations.rb
+++ db/migrate/20120303012729_create_relations.rb
@@ -1,7 +1,11 @@
 class CreateRelations < ActiveRecord::Migration
   def change
     create_table :relations do |t|
-
+      # class
+      t.integer :class
+      # prereqs for that class
+      t.integer :prereq
+      
       t.timestamps
     end
   end
