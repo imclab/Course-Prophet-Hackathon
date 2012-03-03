2d6420c113120964ac95ec58430ea46bec8aaa97
Zack Grannan <zgrannan@gmail.com>
2012-03-02 16:03:53 -0800

    Rails Project Created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseProphet::Application.config.secret_token = 'bba772cecd38bdcb5847b207d79209913bb94bd9a0b87074f2e22570a99f52f698c07b6406b6b7b06de24ab3688e696bda9eee5666edb9f3182f6516de64d568'
