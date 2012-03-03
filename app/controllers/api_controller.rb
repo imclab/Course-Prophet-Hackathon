ea12c2d82144438d39ebb5a3e0759b3770ce4324
Zack Grannan <zgrannan@gmail.com>
2012-03-03 13:17:44 -0800

    GOOD!

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -31,7 +31,6 @@ class ApiController < ApplicationController
             Rails.logger.info("BOO #{relation.prereq} #{taken['id']}")
           end
         end
-
         if !can_take
           taken.each do |taken|
             if relation.prereq == taken
@@ -67,10 +66,6 @@ class ApiController < ApplicationController
     if !params.key?(:classestaken)
       params[:classestaken] = []
     end
-
-    if !params.key?(:units)
-      params[:units] = '12'
-    end
     
     db_courses = Major.where("name='Computer Science, B.S'").first.courses.all
     courses = Array.new
@@ -88,34 +83,8 @@ class ApiController < ApplicationController
       if skip
         next
       else
-        if !course.concurrency.nil? && Course.where("concurrency = ? AND name = 'DUMMY'",course.concurrency).count == 0
-          concurrent_id = course.concurrency
-          concurrencies = Course.where('concurrency = ?',concurrent_id).all
-          units = concurrencies.map{|concourse| concourse.units}.sum
-          joint_course = Course.create(concurrency: concurrent_id,
-                                    name: "DUMMY",
-                                    number: "DUMMY",
-                                    units: units)
-          concurrencies.each do |concourse|
-            prereqs = Relation.where('course = ?',concourse.id).all.map{|r| r.prereq}
-            postreqs = Relation.where('prereq = ?',concourse.id).all.map{|r| r.course}
-            prereqs.each do |prereq_id|
-              Relation.create(prereq: prereq_id, course: joint_course.id )
-            end
-            postreqs.each do |postreq_id|
-              Relation.create(prereq: joint_course.id, course: postreq_id )
-            end
-            #raise Exception
-          end
-          Course.where("concurrency = ? AND name NOT EQUALS 'DUMMY'",course.concurrency).each do |c|
-            Relation.where('course = ?',c.id).destroy_all
-            Relation.where('prereq = ?',c.id).destroy_all
-          end
-          courses.push(joint_course.id)
-        elsif course.concurrency.nil?
-          courses.push(course.id)
-        end
 
+        # PSEUDO CODE FOR CONCURRENCY
         # if concurrency id of current element != unique
         #   get all elements with same concurrent id
         #   course = Course.new(:concurrencyId => nonunique concurrentId, 
@@ -130,61 +99,38 @@ class ApiController < ApplicationController
         #      create new relations for dummy element( dummyCourseId, postreq )
         #   end loop, dummy element should now have relations of all the total
         #             prereqs and postreqs the elements its substituting for
+
+        courses.push(course.id)
       end
-    end 
+    end
+    #render :json => {:courses => db_courses}
+    #return 
     list = top(courses,Array.new,0,params[:classestaken].map{|x| x.to_i})
     list.sort_by {|x| x['level']}
     quarters = Array.new
     quarters[0] = Array.new
     j = 0
     i = 1
-    units_per_quarter = params[:units].to_i
-    units = 0
     quarters[0].push(Course.find(list[0]['id']))
     while i < list.count
       Rails.logger.info('I: #{i}')
+      if ( quarters[j].length > 3 )
+        j+= 1
+        quarters[j] = Array.new
+      end
       if list[i]['level'] > list[i-1]['level'] && quarters[j].length > 0
         j += 1
         quarters[j] = Array.new
-        units = 0
       end
-      if units + Course.find(list[i]['id']).units > units_per_quarter
-        Rails.logger.info("UNITS: #{units + Course.find(list[i]['id']).units}")
-        j += 1
-        quarters[j] = Array.new
-        units = 0
-      end
-
-          # if result of Course.find.name == dummy
-    # concurrencyElementsArray = use concurrency id of dummy to get all coreq courses
-    # push it in
-      course = Course.find(list[i]['id'])
-      if course.name == "DUMMY"
-        requiredUnits = course.units
-        concurrents = Course.where("concurrency=? and name NOT EQUALS 'dummy'",
-                                   course.concurrency).all
-        if units + requiredUnits > units_per_quarter
-          j += 1
-          quarters[j] = Array.new
-          units = 0
-        end
-        concurrents.each do |concur|
-          quarters[j].push(concur)
-        end
-        units += requiredUnits
-        i += 1
-      else
-        quarters[j].push(course)
-        units += Course.find(list[i]['id']).units
-        i += 1
-      end
+      # if result of Course.find.name == dummy
+      # concurrencyElementsArray = use concurrency id of dummy to get all coreq courses
+      # push it in
+      quarters[j].push(Course.find(list[i]['id']))
+      i += 1
     end
 
     quarters.each do |quarter|
-      units = quarter.map{|c| c.units}.sum
-      difference = ((units_per_quarter - units)/4).to_i
-      Rails.logger.info("UNITS #{units} / #{units_per_quarter}")
-      Rails.logger.info("DIFFERENCE #{difference}")
+      difference = 4 - quarter.count
       difference.times do |i|
         quarter.push({:name => 'General Education',
                       :description => 'A general education course',
